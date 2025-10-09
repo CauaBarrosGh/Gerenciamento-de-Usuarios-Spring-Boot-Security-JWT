e93545de85c42c6c511a376c071584f99afc331f
Cauã Barros <clagamesbr01@gmail.com>
2025-10-09 16:10:44 -0300

    ajustes finais do rabbitmq

--- src/test/java/com/estudos/learning_spring_boot/LearningSpringBootApplicationTests.java
+++ src/test/java/com/estudos/learning_spring_boot/LearningSpringBootApplicationTests.java
@@ -3,11 +3,10 @@ package com.estudos.learning_spring_boot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = { "spring.amqp.rabbit.enabled=false" })
 class LearningSpringBootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
--- src/test/java/com/estudos/learning_spring_boot/service/UsuarioServiceTest.java
+++ src/test/java/com/estudos/learning_spring_boot/service/UsuarioServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,6 +22,9 @@ class UsuarioServiceTest {
 
     @Mock
     private UsuarioRepository usuarioRepository;
+
+    @Mock
+    private RabbitTemplate rabbitTemplate;
 
     @InjectMocks
     private UsuarioService usuarioService;

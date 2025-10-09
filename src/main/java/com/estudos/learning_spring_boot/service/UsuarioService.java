0451f8b765e1184e8a8df6f06c1150f325079d25
Cauã Barros <clagamesbr01@gmail.com>
2025-10-09 15:54:04 -0300

    incluindo RabbitMQ

--- src/main/java/com/estudos/learning_spring_boot/service/UsuarioService.java
+++ src/main/java/com/estudos/learning_spring_boot/service/UsuarioService.java
@@ -2,20 +2,25 @@ package com.estudos.learning_spring_boot.service;
 
 import com.estudos.learning_spring_boot.Usuario;
 import com.estudos.learning_spring_boot.repository.UsuarioRepository;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service // Marca esta classe como um Bean de Serviço
+@Service
 public class UsuarioService {
 
-    @Autowired // Spring, por favor INJETE uma instância de UsuarioRepository aqui.
+    @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
     public Usuario criarUsuario(Usuario usuario) {
-        // Aqui poderiam entrar regras de negócio, como validar o email, etc.
-        return usuarioRepository.save(usuario);
+        Usuario usuarioSalvo = usuarioRepository.save(usuario);
+        rabbitTemplate.convertAndSend("usuarios.novos", usuarioSalvo.getEmail());
+        return usuarioSalvo;
     }
 
     public List<Usuario> listarTodos() {
@@ -29,22 +34,17 @@ public class UsuarioService {
     public boolean deletarUsuario(Long id) {
         if (usuarioRepository.existsById(id)) {
             usuarioRepository.deleteById(id);
-            return true; // Retorna true para indicar sucesso.
+            return true;
         }
         return false;
     }
 
     public Optional<Usuario> atualizarUsuario(Long id, Usuario usuarioAtualizado) {
-        // Primeiro, verificamos se o usuário com este ID realmente existe
         return usuarioRepository.findById(id)
                 .map(usuarioExistente -> {
-                    // Se o usuário existir (dentro do .map), atualizamos seus campos.
                     usuarioExistente.setNome(usuarioAtualizado.getNome());
                     usuarioExistente.setEmail(usuarioAtualizado.getEmail());
-
-                    // Salvamos o usuário atualizado. O 'save' funciona como um 'update'
-                    // porque o 'usuarioExistente' já tem um ID.
                     return usuarioRepository.save(usuarioExistente);
-                }); // Se findById não encontrar nada, ele retorna um Optional vazio, e o .map é ignorado.
+                });
     }
 }
